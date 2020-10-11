96d49e5aa8e62e4ae445addee99ac32afe421a2e
Daute Rodríguez Rodríguez <dauterguezrguez@gmail.com>
2020-10-11 18:08:21 +0100

    [auth] Test: setup

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,24 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { app } from '../app';
+
+let mongo: any;
+beforeAll(async () => {
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+  collections.forEach(collection => collection.deleteMany({}));
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});

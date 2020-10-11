adbe37246b3cf823dcb79eb1dfe3d454453a28b1
Daute Rodríguez Rodríguez <dauterguezrguez@gmail.com>
2020-10-11 13:25:54 +0100

    [auth] Feat: current user and signout

--- auth/src/routes/current-user.route.ts
+++ auth/src/routes/current-user.route.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!');
+  const token = req.session ? (req.session.jwt as string) ?? '' : '';
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (err) {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };

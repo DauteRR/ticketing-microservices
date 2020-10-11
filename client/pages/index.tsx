8073447af313214712f370407317c8b982b184d8
Daute Rodríguez Rodríguez <dauterguezrguez@gmail.com>
2020-10-11 20:03:24 +0100

    [client] NextJS setup

---
+++ client/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+export const Index: React.FC = () => {
+  return (
+    <Layout title="Home">
+      <h1>Hello Next.js 👋</h1>
+    </Layout>
+  );
+};
+export default Index;

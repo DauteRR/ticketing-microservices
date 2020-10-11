4d95789be00d94c0181cf99ba60b6579cd93997d
Daute Rodríguez Rodríguez <dauterguezrguez@gmail.com>
2020-10-11 11:43:46 +0100

    [auth] Feat: signin

--- auth/src/routes/signin.route.ts
+++ auth/src/routes/signin.route.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import { SignInDto } from '../dtos/signin.dto';
+import { BadRequestError } from '../errors/bad-request.error';
 import { requestValidator } from '../middlewares/request-validator.middleware';
+import { User } from '../models/user.model';
+import { PasswordUtils } from '../utils/password.utils';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -11,8 +16,32 @@ router.post(
     body('password').trim().notEmpty().withMessage('You must supply a password')
   ],
   requestValidator,
-  (req: Request, res: Response) => {
-    res.send({});
+  async (req: Request<{}, {}, SignInDto>, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const passwordsMatch = await PasswordUtils.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!passwordsMatch) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const userJwt = jwt.sign(
+      { email: existingUser.email, id: existingUser._id },
+      process.env.JWT_KEY!
+    );
+
+    req.session = { jwt: userJwt };
+
+    res.status(200).send(existingUser);
   }
 );
 

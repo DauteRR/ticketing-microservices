a41d1d86875a8f76938110fca1898df6929aa4d5
Daute Rodríguez Rodríguez <dauterguezrguez@gmail.com>
2020-10-24 08:58:38 +0100

    Ref: DB_URI as env variable
    - [k8s & auth & client]

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,9 +5,12 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY environment variable must be defined');
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI environment variable must be defined');
+  }
 
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true

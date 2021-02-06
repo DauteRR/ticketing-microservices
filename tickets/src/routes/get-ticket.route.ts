3bfbe20f01fe319b3272c8c027c3589bb85d8cb2
Daute Rodríguez Rodríguez <dauterguezrguez@gmail.com>
2021-02-06 10:02:50 +0000

    [tickets] Fix: routes naming

--- tickets/src/routes/get-ticket.route.ts
+++ tickets/src/routes/get-ticket.route.ts
@@ -1,13 +1,28 @@
+import { NotFoundError, requestValidator } from '@drrtickets/common';
 import express, { Request, Response } from 'express';
-import { GetTicketsResponse } from '../dtos/tickets.dto';
+import { param } from 'express-validator';
+import { CreateTicketResponse } from '../dtos/tickets.dto';
 import { Ticket } from '../models/ticket.model';
 
 export const getTicketRouter = express.Router();
 
 getTicketRouter.get(
-  '/api/tickets',
-  async (req: Request, res: Response<GetTicketsResponse>) => {
-    const tickets = await Ticket.find();
-    res.send(tickets);
+  '/api/tickets/:id',
+  [
+    param('id')
+      .isMongoId()
+      .withMessage('Wrong format, a MongoDB id was expected')
+  ],
+  requestValidator,
+  async (req: Request<{ id: string }>, res: Response<CreateTicketResponse>) => {
+    const { id } = req.params;
+
+    const ticket = await Ticket.findById(id);
+
+    if (!ticket) {
+      throw new NotFoundError('GET', `/api/tickets/${id}`);
+    }
+
+    res.send(ticket);
   }
 );
--- tickets/src/routes/get-tickets.route.ts
+++ tickets/src/routes/get-tickets.route.ts
@@ -1,28 +1,13 @@
-import { NotFoundError, requestValidator } from '@drrtickets/common';
 import express, { Request, Response } from 'express';
-import { param } from 'express-validator';
-import { CreateTicketResponse } from '../dtos/tickets.dto';
+import { GetTicketsResponse } from '../dtos/tickets.dto';
 import { Ticket } from '../models/ticket.model';
 
 export const getTicketsRouter = express.Router();
 
 getTicketsRouter.get(
-  '/api/tickets/:id',
-  [
-    param('id')
-      .isMongoId()
-      .withMessage('Wrong format, a MongoDB id was expected')
-  ],
-  requestValidator,
-  async (req: Request<{ id: string }>, res: Response<CreateTicketResponse>) => {
-    const { id } = req.params;
-
-    const ticket = await Ticket.findById(id);
-
-    if (!ticket) {
-      throw new NotFoundError('GET', `/api/tickets/${id}`);
-    }
-
-    res.send(ticket);
+  '/api/tickets',
+  async (req: Request, res: Response<GetTicketsResponse>) => {
+    const tickets = await Ticket.find();
+    res.send(tickets);
   }
 );

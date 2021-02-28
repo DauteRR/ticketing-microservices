e05be69149c738931cb46bb62efe624e0edd05c7
Daute Rodríguez Rodríguez <dauterguezrguez@gmail.com>
2021-02-28 18:08:18 +0000

    [payments] Payments service initial setup

---
+++ payments/src/app.ts
@@ -0,0 +1,24 @@
+import { currentUser, errorHandler, NotFoundError } from '@drrtickets/common';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import express, { Request } from 'express';
+import 'express-async-errors';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+  })
+);
+app.use(currentUser);
+
+app.all('*', async (req: Request) => {
+  throw new NotFoundError(req.method, req.path);
+});
+
+app.use(errorHandler);
+
+export { app };

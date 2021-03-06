044548d6b827f9f156c93918625aa21a002beeea
Daute Rodríguez Rodríguez <dauterguezrguez@gmail.com>
2021-03-06 18:43:30 +0000

    [common] Del: unnecesary order statuses

--- common/src/types/order.ts
+++ common/src/types/order.ts
@@ -1,8 +1,6 @@
 export enum OrderStatus {
   Created = 'created',
   Canceled = 'canceled',
-  Expired = 'expired',
-  Unavailable = 'unavailable',
   AwaitingPayment = 'awaiting:payment',
   Complete = 'complete'
 }
